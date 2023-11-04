2124ba7bccdc9940b03c7349a1c4adf93c1f0647
ugyentring <12220096.gcit@rub.edu.bt>
2023-11-04 12:55:46 +0600

    react router project

--- reactRouter/src/components/Home/Home.jsx
+++ reactRouter/src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
-import Link from "react-router-dom";
+import {Link} from "react-router-dom";
 
-export default function Home() {
+function Home() {
   return (
     <div className="mx-auto w-full max-w-7xl">
       <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
@@ -53,3 +53,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
